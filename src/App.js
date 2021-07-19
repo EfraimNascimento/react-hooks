32806284aad997a337f7285c189995dcb774ed2d
Efraim Nascimento <efraimteofilocamargonascimento@outlook.com>
2021-07-18 22:26:01 -0300

    react-hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default function App(){
+
+  const [location, setLocation] = useState({});
+
+  useEffect(() =>{
+    const watchId = navigator.geolocation.watchPosition(handlePositionReceived)
+
+    return () => navigator.geolocation.clearWatch(watchId);
+  }, []);
+
+  function handlePositionReceived({ coords }){
+    const { latitude, longitude } = coords;
+
+    setLocation({ latitude, longitude })
+  }
+
+  // const [repositories, setRepositories] = useState([]);
+
+  // useEffect(async() => {
+  //   const response = await fetch('https://api.github.com/users/efraimnascimento/repos');
+  //   const data =await response.json();
+
+  //   setRepositories(data);
+  // }, []);
+
+  // useEffect(() =>{
+  //   const filtered = repositories.filter(repo => repo.favorite);
+
+  //   document.title = `Você tem ${filtered.length} repositórios favoritos`
+  // }, [repositories]);
+
+  // function handleFavorite(id){
+  //   const newRepositories = repositories.map(repo =>{
+  //     return repo.id === id ? {...repo, favorite: !repo.favorite} : repo 
+  //   })
+
+  //   setRepositories(newRepositories);
+  // }
+
+  return(
+    <>
+
+      Latitude: {location.latitude} <br />
+      Longitude: {location.longitude}
+
+      {/* <ul>
+        {repositories.map(repo =>(
+          <li 
+          key={repo.id}>
+            {repo.name} {
+              repo.favorite && <span>(Favorito)</span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+            </li>
+        ))}
+      </ul> */}
+    </>
+  )
 }
-
-export default App;
